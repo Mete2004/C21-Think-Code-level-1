eeb225fd3b641766614ed0e49221b7cf9f1d1d04
Mete2004 <2150982@talnet.nl>
2021-11-22 11:53:21 +0100

    eindopdracht python opnieuw gemaakt

--- Eindopdracht/Eindopdracht.py
+++ Eindopdracht/Eindopdracht.py
@@ -1,83 +1,50 @@
-# -------------------------
-def new_game():
+print('Welcome to Mete his quiz')
+answer=input('Are you ready to play my first Quiz ? (yes/no) :')
+score=0
+total_questions=5
+ 
 
-    guesses = []
-    correct_guesses = 0
-    question_num = 1
 
-    for key in questions:
-        print("-------------------------")
-        print(key)
-        for i in options[question_num-1]:
-            print(i)
-        guess = input("Enter (A, B, C, or D): ")
-        guess = guess.upper()
-        guesses.append(guess)
+if answer.lower()=='yes':
+  
+    answer=input('Question 1: What do you know about Python?')
+  
+    if answer.lower()=='yes':
+        score += 1
+        print('correct')
 
-        correct_guesses += check_answer(questions.get(key), guess)
-        question_num += 1
-
-    display_score(correct_guesses, guesses)
-
-# -------------------------
-def check_answer(answer, guess):
-
-    if answer == guess:
-        print("GOED!")
-        return 1
+ 
+    
+    answer=input('Question 2: Have you ever coded with Python, if so how often? ')
+   
+    if answer.lower()=='yes':
+        score += 1
+        print('correct')
+   
+ 
+    
+    answer=input('Question 3: Do you like coding? ')
+    if answer.lower()=='yes':
+        score += 1
+        print('correct')
+    
+    
+    
+    answer=input('Question 4: What is your feedback on my first quiz? ')
+    if answer.lower()=='yes':
+        score += 1
+        print('correct')
+   
+    
+    answer=input('Question 5: what color is a banana? ')
+    if answer.lower()=='yellow':
+        score += 1
+        print('correct')
     else:
-        print("FOUT!")
-        return 0
+        print('Wrong Answer :(')
 
-# -------------------------
-def display_score(correct_guesses, guesses):
-    print("-------------------------")
-    print("Resultaten")
-    print("-------------------------")
-
-    print("Antwoorden: ", end="")
-    for i in questions:
-        print(questions.get(i), end=" ")
-    print()
-
-    print("raad: ", end="")
-    for i in guesses:
-        print(i, end=" ")
-    print()
-
-    score = int((correct_guesses/len(questions))*100)
-    print("Je score is: "+str(score)+"%")
-
-# -------------------------
-def play_again():
-
-    response = input("Wil je opnieuw spelen? (JA of NEE): ")
-    response = response.upper()
-
-    if response == "Correct":
-        return True
-    else:
-        return False
-# -------------------------
-
-
-questions = {
- "Wat is mijn naam?: ": "D",
- "Hoe oud ben ik?: ": "C",
- "Hoe lang is de Eiffeltoren?: ": "D",
- "Wie is de bedenker van de Eiffeltoren?: ": "C"
-}
-
-options = [["A. Ahmed hari", "B. Joop", "C. Mohammed", "D. Mete Sariguney"],
-          ["A. 14", "B. 19", "C. 16", "D. 21"],
-          ["A. 255m", "B. 300m", "C. 350m", "D. 324m"],
-          ["A. Ali","B. Michael Jackson", "C. Gustavo Eiffel", "D. Ik weet het niet"]]
-
-new_game()
-
-while play_again():
-    new_game()
-
-print("Einde!")
-
-# -------------------------
+ 
+print('Thankyou for Playing my first quiz game, im super glad you played my first quiz. you attempted',score,"questions correctly!")
+mark=(score/total_questions)*100
+print('Marks obtained:',mark)
+print('BYE!')
