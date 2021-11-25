ec6552280e6b824f3cfc5d9f228554b287301c30
Mete2004 <2150982@talnet.nl>
2021-11-25 09:18:46 +0100

    py.2 opnieuw bekeken en verbetert.

--- Opdracht2/opdracht2.py
+++ Opdracht2/opdracht2.py
@@ -1,9 +1,17 @@
 # syntax error
+
 print("Hello world")
 
+
+
 # runtime error
-10 * ("2/0")
+
+10 * (2/1)
+
+
 
 # semantic error
-name = ("Alice")
-print("Hello name")
+
+name = "Alice"
+
+print("Hello",name)
